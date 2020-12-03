7c7771eee4220a5cec467fbb276d4140d11c3718
Jesse Swedlund <swedlundjesse@gmail.com>
2020-12-03 09:31:25 -0800

    fixed enemies.get bug

--- client/src/helpers/enemy.js
+++ client/src/helpers/enemy.js
@@ -1,32 +1,32 @@
 export default new Phaser.Class({
-	Extends: Phaser.GameObjects.Sprite,
+  Extends: Phaser.GameObjects.Sprite,
 
-	initialize: function Enemy(scene) {
-		console.log('starting initialization of enemy...');
-		this.createdByPlayerA = scene.event;
-		if (this.createdByPlayerA) {
-			//if playerA hit the keyboard - create a p1 attacker
+  initialize: function Enemy(scene) {
+    console.log("starting initialization of enemy...");
+    this.createdByPlayerA = scene.event;
+    if (this.createdByPlayerA) {
+      //if playerA hit the keyboard - create a p1 attacker
       Phaser.GameObjects.Sprite.call(this, scene, 125, 240, "p1attackers");
       this.follower = { t: 0, vec: new Phaser.Math.Vector2() };
       scene.enemyNumber++;
       this.number = scene.enemyNumber;
     } else {
-      Phaser.GameObjects.Sprite.call(this, scene, 655, 240, "p2attackers");
-      this.follower = { t: 1, vec: new Phaser.Math.Vector2() };
+      Phaser.GameObjects.Sprite.call(this, scene, 500, 240, "p2attackers");
+      this.follower = { t: 0.5, vec: new Phaser.Math.Vector2() };
       scene.enemyNumber++;
       this.number = scene.enemyNumber;
     }
   },
-  
+
   //differentiate player attacks based on class
   //if playerA then create a playera attacker else create a playerb attacker
-  
+
   startOnPath: function (path) {
     if (this.createdByPlayerA) {
       this.path = path;
       // set the t parameter at the start of the path
       this.follower.t = 0;
-      this.anims.play('walk')
+      this.anims.play("blueWalk");
       // get x and y of the given t point
       // console.log(this.path.getPoint(this.follower.t, this.follower.vec));
       this.path.getPoint(this.follower.t, this.follower.vec);
@@ -36,9 +36,8 @@ export default new Phaser.Class({
       console.log(this.hp);
     } else {
       this.path = path;
-      this.follower.t = 1;
-      this.anims.play('walk')
-
+      this.follower.t = 0.5;
+      this.anims.play("redWalk");
 
       this.path.getPoint(this.follower.t, this.follower.vec);
       // set the x and y of our enemy to the received from the previous step
@@ -60,27 +59,27 @@ export default new Phaser.Class({
     }
   },
 
-	update: function (time, delta) {
-		if (this.path) {
-			this.path.getPoint(this.follower.t, this.follower.vec);
+  update: function (time, delta) {
+    if (this.path) {
+      this.path.getPoint(this.follower.t, this.follower.vec);
 
-			this.setPosition(this.follower.vec.x, this.follower.vec.y);
+      this.setPosition(this.follower.vec.x, this.follower.vec.y);
 
-			if (this.createdByPlayerA) {
-				this.follower.t += this.scene.SCISSOR_SPEED * delta;
+      if (this.createdByPlayerA) {
+        this.follower.t += this.scene.SCISSOR_SPEED * delta;
 
-				if (this.follower.t >= 1) {
-					this.setActive(false);
-					this.setVisible(false);
-				}
-			} else {
-				this.follower.t -= this.scene.SCISSOR_SPEED * delta;
+        if (this.follower.t >= 1) {
+          this.setActive(false);
+          this.setVisible(false);
+        }
+      } else {
+        this.follower.t -= this.scene.SCISSOR_SPEED * delta;
 
-				if (this.follower.t <= 0) {
-					this.setActive(false);
-					this.setVisible(false);
-				}
-			}
-		}
-	},
+        if (this.follower.t <= 0) {
+          this.setActive(false);
+          this.setVisible(false);
+        }
+      }
+    }
+  },
 });
