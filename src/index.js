f45d33b1aa2d3c612dd15034a9b309e61e3f2197
Andrew Cohen <andrcohen847@gmail.com>
2020-12-03 16:49:12 -0600

    intro page working and additional css styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Phaser from "phaser";
 import Game from "./scenes/game";
 import SceneTwo from "./scenes/gameOver"
+import IntroScene from "./scenes/intro"
 
 const config = {
   type: Phaser.AUTO,
@@ -10,7 +11,13 @@ const config = {
   physics: {
     default: "arcade",
   },
-  scene: [Game, SceneTwo],
 };
 
 const game = new Phaser.Game(config);
+
+//load scenes
+game.scene.add('introScene', IntroScene)
+game.scene.add("game", Game)
+
+//start title
+game.scene.start('introScene')
