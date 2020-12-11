2edd77cded044a8b8be8989d341838aaa9b7a407
Min Kyu Han <minkyu.han1029@gmail.com>
2020-12-11 11:29:40 -0500

    changed color

--- src/helpers/attacker.js
+++ src/helpers/attacker.js
@@ -60,7 +60,7 @@ export default new Phaser.Class({
         this.healthBar = this.scene.makeBar(
           this.follower.vec.x - 10,
           this.follower.vec.y - 20,
-          0x2ecc71
+          0x008ee2
         );
         this.hp = 80;
       }
@@ -70,7 +70,7 @@ export default new Phaser.Class({
         this.healthBar = this.scene.makeBar(
           this.follower.vec.x - 20,
           this.follower.vec.y - 20,
-          0x2ecc71
+          0x008ee2
         );
         this.hp = 100;
       }
@@ -80,7 +80,7 @@ export default new Phaser.Class({
         this.healthBar = this.scene.makeBar(
           this.follower.vec.x - 30,
           this.follower.vec.y - 20,
-          0x2ecc71
+          0x008ee2
         );
         this.hp = 120;
       }
